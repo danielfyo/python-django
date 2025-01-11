4b150e006a07e3777bb13ae794b559960ba4f996
Daniel Yepes O <danielfyo@icloud.com>
2025-01-11 12:43:59 +0100

    example of how to get the type of variable

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 ﻿print("Starting e-commerce...")
 action_name = input("Type the action name to continue: ")
-print("Selected option: " + action_name)
+print("Selected option: " + action_name + ", type: ")
+print(type(action_name))
+
+print("1 is a:")
+print(type(1))
+print("1.1 is a:")
+print(type(1.1))
